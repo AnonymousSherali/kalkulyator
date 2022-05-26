605c6aa42bbece3a61650b690592524b3830b925
Anonim <72141730+AnonymousSherali@users.noreply.github.com>
2022-05-26 21:34:15 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,39 @@
+import tkinter as tk
+
+def add_digit(digit):
+    value = calc.get() + str(digit)
+    calc.delete(0, tk.END)
+    calc.insert(0, value)
+    
+def make_digit_button(digit):
+    return tk.Button(text=digit, bd=5, font=('Hack', 13), command=lambda : add_digit(digit))
+
+win = tk.Tk()
+win.geometry(f"240x260+100+200")
+win['bg'] = '#33ffe6'
+win.title('Kalkulyator')
+
+calc = tk.Entry(win, justify=tk.RIGHT, font=('Hack', 15), width=15)
+calc.grid(row=0, column=0, columnspan=3, sticky='we')
+
+make_digit_button('1').grid(row=1, column=0, sticky='wens', padx=5, pady=5)
+make_digit_button('2').grid(row=1, column=1, sticky='wens', padx=5, pady=5)
+make_digit_button('3').grid(row=1, column=2, sticky='wens', padx=5, pady=5)
+make_digit_button('4').grid(row=2, column=0, sticky='wens', padx=5, pady=5)
+make_digit_button('5').grid(row=2, column=1, sticky='wens', padx=5, pady=5)
+make_digit_button('6').grid(row=2, column=2, sticky='wens', padx=5, pady=5)
+make_digit_button('7').grid(row=3, column=0, sticky='wens', padx=5, pady=5)
+make_digit_button('8').grid(row=3, column=1, sticky='wens', padx=5, pady=5)
+make_digit_button('9').grid(row=3, column=2, sticky='wens', padx=5, pady=5)
+make_digit_button('0').grid(row=4, column=0, sticky='wens', padx=5, pady=5)
+
+win.grid_columnconfigure(0, minsize=60)
+win.grid_columnconfigure(1, minsize=60)
+win.grid_columnconfigure(2, minsize=60)
+
+win.grid_rowconfigure(1, minsize=60)
+win.grid_rowconfigure(2, minsize=60)
+win.grid_rowconfigure(3, minsize=60)
+win.grid_rowconfigure(4, minsize=60)
+
+win.mainloop()
